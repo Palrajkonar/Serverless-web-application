8893e596333fed80cf51aca5296db50c221967a9
Palrajkonar <145372560+Palrajkonar@users.noreply.github.com>
2024-05-07 19:54:09 +0530

    Update script.js

--- AWS/script.js
+++ AWS/script.js
@@ -11,7 +11,7 @@ function submitForm() {
     const xhr = new XMLHttpRequest();
 
     // Set up request
-    xhr.open('POST', '/register', true);
+    xhr.open('POST', 'https://64o4jndh1k.execute-api.ap-south-1.amazonaws.com/test/register', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     // Set up response handler
